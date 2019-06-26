0126c112007d878b53a6aa0980c9e186955ab3bb
= <Kapustlo@yandex.ru>
2019-06-26 04:09:17 +0300

    Handler is now a class

--- server/handler.py
+++ server/handler.py
@@ -1,53 +1,58 @@
 import json
 
-def validate_value(value):
-    try:
-        value = value.decode("utf-8")
-    except:
-        value = str(value)
+class Handler:
+    def __init__(self, charset="utf-8"):
+        super(, self).__init__()
+        self.charset = charset
 
-    return value
+    def validate_value(self, value):
+        try:
+            value = value.decode(self.charset)
+        except:
+            value = str(value)
 
-def incorrect_json(value=""):
-    value = validate_value(value)
-    return json.dumps({
-        "status": "error",
-        "fatal": True,
-        "text": "Invalid json format",
-        "value": value
-    }).encode("utf-8")
+        return value
 
-def username_not_unique(username=""):
-    username = validate_value(username)
-    return json.dumps({
-        "status": "error",
-        "fatal": True,
-        "text": "Username is not unique",
-        "value": username
-    }).encode("utf-8")
+    def incorrect_json(self, value=""):
+        value = validate_value(value)
+        return json.dumps({
+            "status": "error",
+            "fatal": True,
+            "text": "Invalid json format",
+            "value": value
+        }).encode(self.charset)
 
-def failed_cridentials(value=""):
-    value = validate_value(value)
-    return json.dumps({
-        "status": "error",
-        "fatal": True,
-        "text": "Incorrect cridentials",
-        "value": value
-    }).encode("utf-8")
+    def username_not_unique(self, username=""):
+        username = validate_value(username)
+        return json.dumps({
+            "status": "error",
+            "fatal": True,
+            "text": "Username is not unique",
+            "value": username
+        }).encode(self.charset)
 
-def max_conns_ecc():
-    return json.dumps({
-        "status": "error",
-        "fatal": True,
-        "text": "Could not connect: maximum users reached",
-        "value": ""
-    }).encode("utf-8")
+    def failed_cridentials(self, value=""):
+        value = validate_value(value)
+        return json.dumps({
+            "status": "error",
+            "fatal": True,
+            "text": "Incorrect cridentials",
+            "value": value
+        }).encode(self.charset)
 
-def msg_length_ecc(length=""):
-    length = validate_value(length)
-    return json.dumps({
-        "status": "error",
-        "fatal": False,
-        "text": "Message is too long, maximum length is " + length,
-        "value": ""
-    }).encode("utf-8")
+    def max_conns_ecc(self):
+        return json.dumps({
+            "status": "error",
+            "fatal": True,
+            "text": "Could not connect: maximum users reached",
+            "value": ""
+        }).encode(self.charset)
+
+    def msg_length_ecc(self, length=""):
+        length = validate_value(length)
+        return json.dumps({
+            "status": "error",
+            "fatal": False,
+            "text": "Message is too long, maximum length is " + length,
+            "value": ""
+        }).encode(self.charset)
